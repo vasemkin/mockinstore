f9733bcba763a436f3128e3ddbf70ff671c2aa52
vasemkin <vasemkin@ya.ru>
2021-04-19 22:55:04 +0300

    added backend

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+module.exports = function(){
+    const faker = require('faker');
+    const _ = require('lodash');
+    return {
+        items : _.times(300, function(n){
+            return {
+                id : n,
+                productName : faker.commerce.productName(),
+                price : faker.commerce.price(),
+                department : faker.commerce.department(),
+                color : faker.commerce.color(),
+                productMaterial : faker.commerce.productMaterial(),
+                productDescription : faker.commerce.productDescription(),
+                image : faker.image.abstract()
+            }
+        })
+    }
+}
